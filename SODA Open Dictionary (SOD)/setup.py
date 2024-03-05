9ee5b35e00c21e7ceb60351ed93bcabe66916c9e
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-03-05 23:06:23 +0700

    Improved performance, crash handler has been added
    Version 1.0.0dev - Performance Enhancement and Crash Handling
    
    1 Improved Performance:
    *We’ve optimized the codebase to enhance application performance.
    *This improvement ensures a smoother experience when using search features and browsing the web.
    2 Added Crash Handler:
    *We’ve integrated a crash handler to address issues related to search functionality when an internet connection is available.
    *When the application encounters a problem (e.g., unexpected closure), the crash handler collects relevant information (such as error logs) and may attempt graceful recovery or provide useful feedback to developers.
    
    If you encounter frequent issues, consider disabling any optional features related to crash reporting or performance enhancement.
    
    Please note that crash handlers are essential for diagnosing and resolving issues, but they can sometimes impact performance. Balancing these aspects ensures a seamless user experience. 🚀

---
+++ SODA Open Dictionary (SOD)/setup.py
@@ -0,0 +1,52 @@
+import os
+import zipfile
+import io
+import shutil
+
+try:
+    import requests
+    import winshell
+except:
+    os.system("pip install pywin32 winshell requests")
+    os.system("setup.py")
+else:
+    try:
+        os.system("pip install kivymd==1.2.0 kivy googletrans==4.0.0rc1 eng-to-ipa pyttsx3 notify-py")
+        os.system("python.exe -m pip install --upgrade pip")
+
+        repo_url = "https://github.com/soda2611/SODA_Open_Dictionary/archive/refs/heads/main.zip"
+
+        repo_dir = "SODA_Open_Dictionary-main"
+
+        sod_dir = "SODA Open Dictionary (SOD)"
+
+        response = requests.get(repo_url)
+
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+
+        zip_file.extractall()
+
+        shutil.move(os.path.join(repo_dir, sod_dir), sod_dir)
+
+        os.system(f"rmdir /S /Q {repo_dir}")
+
+        python_file = os.path.abspath(f"{sod_dir}/UI.py")
+
+        icon_file = os.path.abspath(f"{sod_dir}/func/Logo.ico")
+
+        desktop = winshell.desktop()
+
+        shortcut_name = "SODA Open Dictionary"
+
+        shortcut = os.path.join(desktop, shortcut_name + ".lnk")
+
+        with winshell.shortcut(shortcut) as link:
+            link.path = python_file
+            link.description = "An open dictionary for everyone"
+            link.arguments = python_file
+            link.icon_location = (icon_file, 0)
+            link.working_directory = os.path.abspath(sod_dir)
+        
+    except Exception as ex:
+        print(ex)
+
