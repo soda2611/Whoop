4e9602d4e122afa4de7f606a80b58ff588438124
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-10-05 18:45:16 +0700

    Update data_manager.py

--- Whoop/func/data_manager.py
+++ Whoop/func/data_manager.py
@@ -4,7 +4,6 @@ import requests, base64, os
 token = "g%h%p%_%l%6%b%z%c%z%t%j%6%Q%R%0%q%e%i%W%k%S%l%h%W%U%8%U%4%H%s%6%r%K%0%n%I%K%U%9".replace("%", "")
 repo_url = "https://api.github.com/repos/soda2611/Whoop"
 
-# Tải xuống file
 def download_file(file_path, name):
     url = f"{repo_url}/contents/{file_path}"
     headers = {"Authorization": f"token {token}"}
@@ -18,27 +17,21 @@ def download_file(file_path, name):
     else:
         print(f"Error downloading file: {response.text}")
 
-# Tải lên file
-'''def upload_file(file_path, name):
-    url = f"{repo_url}/contents/{file_path}"
-    with open(name, 'r', encoding="utf-8") as f:
-        content = f.read()
-    headers = {"Authorization": f"token {token}"}
-    data = {'message': 'Update file', 'content': content.encode('base64').decode('utf-8')}
-    response = requests.put(url, headers=headers, json=data)
-    if response.status_code == 201:
-        print("File uploaded successfully")
-    else:
-        print(f"Error uploading file: {response.text}")'''
-        
 def upload_file(file_path, name):
     url = f"{repo_url}/contents/{file_path}"
     with open(name, 'rb') as f:
         content = f.read()
+    encoded_content = base64.b64encode(content).decode('utf-8')
+    
     headers = {"Authorization": f"token {token}"}
-    data = {'message': 'Update file', 'content': base64.b64encode(content).decode('utf-8')}
+    data = {
+        'message': 'Update file',
+        'content': encoded_content
+    }
     response = requests.put(url, headers=headers, json=data)
     if response.status_code == 201:
         print("File uploaded successfully")
+    elif response.status_code == 200:
+        print("File updated successfully")
     else:
-        print(f"Error uploading file: {response.text}")
+        print(f"Error uploading file: {response.status_code} - {response.text}")
