1116192074f5489c781050a910e7164883b63c5e
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-11-30 10:50:46 +0700

    Add files via upload

---
+++ Whoop/func/SOD.py
@@ -0,0 +1,96 @@
+try:
+    from necessary_function import spelling_checker_for_SOD, check_connection, data as word_data
+except:
+    from func.necessary_function import spelling_checker_for_SOD, check_connection, data as word_data
+from googletrans import Translator
+import re, requests
+
+translator=Translator()
+
+def SOD_word_list(path="data/tu_dien_nguon.txt", sub_path="func/data/tu_dien_nguon.txt"):
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            data = f.read()
+    except:
+        with open(sub_path, "r", encoding="utf-8") as f:
+            data = f.read()
+
+    tu_dien = eval(data)
+    
+    return tu_dien
+
+with open("func/data/source.txt", encoding="utf-8") as f: 
+    source=eval(f.read())
+
+def SOD(inp, database='default', internet=check_connection()):
+    if database!='default':
+        with open(database, encoding="utf-8") as f:
+            database=eval(f.read())
+    for word in inp:
+        if database!="default" and word in database:
+            data_=database[word]
+        elif internet:
+            url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+            response = requests.get(url)
+            
+            data_={}
+            if response.status_code == 200:
+                data = response.json()
+                if isinstance(data, list):
+                    word_data = data[0]
+                    meanings = word_data.get("meanings", "none")
+                    for meaning in meanings:
+                        part_of_speech = meaning.get("partOfSpeech", "none")
+                        synonyms = meaning.get("synonyms", "none")
+                        antonyms = meaning.get("antonyms", "none")
+                        data_[part_of_speech]={"definition": "", "synonyms": synonyms, "antonyms": antonyms}
+                        for definition in meaning.get("definitions", "none"):
+                            definition_text = definition.get("definition", "none")
+                            example = definition.get("example", [])
+
+                            if example==[]: example=2*"\n"
+                            else: example=f"\nVí dụ: {example}\n\n"
+                            data_[part_of_speech]["definition"]=data_[part_of_speech]["definition"]+f"- {definition_text}{example}"
+        elif not internet:
+            data_="Không có kết nối mạng và không có sẵn trong bộ dữ liệu offline"
+    return data_ if data_ else "Không tìm thấy từ"
+
+def grammar_structure_detector(inp,file_path):
+    patterns=build_patterns(file_path)
+    result=[]
+    for i in patterns:
+        if len(re.findall(patterns[i], inp))!=0:
+            result.append(i.replace("\n",""))
+    return result
+
+def build_patterns(file_path):
+    patterns = {}
+    with open(file_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            parts,structure = line.split(": ")
+            parts=parts.split('...')
+            pattern = '\s*.*?'.join(f'({part.strip()})' for part in parts)
+            patterns[structure] = pattern
+    return patterns
+
+def word_detector(inp):
+    inp = inp.split()
+    list_ = []
+    i = 0
+    while i < len(inp):
+        max_len_phrase = inp[i]
+        for j in range(i, len(inp)):
+            phrase = " ".join(inp[i:j+1])
+            if phrase in source:
+                if len(phrase) > len(max_len_phrase):
+                    max_len_phrase = phrase
+        if max_len_phrase not in list_:
+            list_.append(max_len_phrase)
+        i += len(max_len_phrase.split())
+    return list_
+
+if __name__=="__main__":
+    print(SOD(["art"]))
---
+++ Whoop/ui/setting/__init__.py
@@ -0,0 +1,216 @@
+import ui as ui
+from ui import *
+from ui.setting.widget.change_palette import change_palette
+from ui.setting.widget.change_fonts import change_fonts
+
+class setting(MDBoxLayout):
+    def __init__(self, **kwargs):
+        super(setting, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.padding = [10, 10, 10, 10]
+        self.spacing=20*scale
+        self.md_bg_color=bg
+        
+        self.overlay=MDCard(size_hint=(1, 1), md_bg_color=bg)
+        self.overlay.bind(on_touch=self.touch_ignore)
+        self.overlay.add_widget(MDLabel(text="Đang cập nhật...", font_style="H6", halign="center", valign="middle", pos_hint={"center_x": 0.5, "center_y": 0.5}, theme_text_color="Custom", text_color=primarycolor))
+
+        self.success=MDDialog(
+            title="Cập nhật thành công.\nKhởi động lại để áp dụng dữ liệu mới",
+            type="alert",
+            md_bg_color=boxbg
+        )
+        self.failed=MDDialog(
+            title="Cập nhật thất bại",
+            type="alert",
+            md_bg_color=boxbg
+        )
+
+        self.search_thread=None
+        self.icon=MDIconButton(icon="check-circle", theme_icon_color="Custom", icon_color=primarycolor, size_hint=(None, None), pos_hint={"center_x": 0.5})
+        self.got_check=None
+        self.got_font_check=None
+        self.touch_count=0
+        self.timer = None
+        self.back_button=MDIconButton(icon="arrow-left", size_hint=(None, None), pos_hint={"left": 0})
+        self.back_button.bind(on_press=self.back)
+        self.scrollview=ScrollView(size_hint=(1,1), do_scroll_x=False)
+        self.personalize=MDBoxLayout(orientation="vertical", size_hint=(1,None), pos_hint={"center_x": 0.5}, spacing=20*scale)
+        self.personalize.bind(minimum_height=self.personalize.setter('height'))
+        self.scrollview.add_widget(self.personalize)
+        self.changecolor=MDLabel(text="Cá nhân hóa với bảng màu", font_style="H6", halign="left", size_hint=(1,None), height=30*scale, pos_hint={"center_x":0.5}, theme_text_color="Custom", text_color=primarycolor)
+        self.color_palette_scroll=change_palette(self.create_palette)
+        self.change_fonts=MDLabel(text="Fonts", font_style="H6", halign="left", size_hint=(1,None), height=30*scale, pos_hint={"center_x":0.5}, theme_text_color="Custom", text_color=primarycolor)
+        self.font_scroll=change_fonts(self.create_preview)
+        self.color_palette_scroll.scroll_to(self.got_check)
+        self.font_scroll.scroll_to(self.got_font_check)
+        self.info=MDFillRoundFlatButton(text=f"Phiên bản: SOD {version}\nNgày phát hành: Unknown    UID: {settings['uid']}", font_style="Caption", halign="center", size_hint=(0.75,None), height=35*scale, pos_hint={"center_x": 0.5}, theme_text_color="Custom", text_color=primarycolor, md_bg_color=bg)
+        self.info.bind(on_release=self.on_touch)
+        self.dialog = MDDialog(
+            title="Cài đặt liên quan đến cá nhân hóa sẽ được áp dụng khi bạn khởi động lại ứng dụng",
+            type="alert",
+            buttons=[
+                MDFillRoundFlatButton(
+                    text="Khởi động lại",
+                    md_bg_color=btn,
+                    theme_text_color="Custom",
+                    text_color=secondarycolor,
+                    on_press=self.restart
+                ),
+                MDFillRoundFlatButton(
+                    text="Đóng",
+                    md_bg_color=btn,
+                    theme_text_color="Custom",
+                    text_color=secondarycolor
+                )
+            ],
+            md_bg_color=boxbg
+        )
+        self.dialog.buttons[1].bind(on_release=self.dialog.dismiss)
+
+        '''self.notification=MDLabel(text="Bạn không thể thay đổi bất cứ cài đặt nào của ứng dụng trong phiên bản này. Vui lòng chờ phiên bản cập nhật tiếp theo.", font_style="H6", halign="center", size_hint=(0.75,1), pos_hint={"center_x": 0.5})
+        self.add_widget(self.notification)'''
+
+        self.add_widget(self.back_button)
+        self.add_widget(self.scrollview)
+        self.personalize.add_widget(self.changecolor)
+        self.personalize.add_widget(self.color_palette_scroll)
+        self.personalize.add_widget(self.change_fonts)
+        self.personalize.add_widget(self.font_scroll)
+        self.add_widget(self.info)
+
+    def restart(self, instance):
+        set_new_config()
+        subprocess.Popen(["py", "UI.py"], start_new_session=True)
+        os.kill(os.getpid(), signal.SIGTERM)
+        
+    def create_palette(self, i):
+        self.color=MDCard(md_bg_color=(1,1,1,1), orientation="vertical", size_hint=(None, None), width=50*scale, height=200*scale, pos_hint={"center_x": 0.5}, padding=[10*scale, 10*scale, 10*scale, 10*scale], ripple_behavior=True)
+        self.color.radius=[i*scale for i in self.color.radius]
+        theme=[]
+        for j in i:
+            theme.append([int(j[0])/255,int(j[1])/255,int(j[2])/255, 1])
+        for j in i[:4]:
+            self.color1=MDBoxLayout(md_bg_color=(int(j[0])/255,int(j[1])/255,int(j[2])/255, 1),size_hint=(1,0.25), pos_hint={"center_x":0.5})
+            self.color.add_widget(self.color1)
+        if [[str(int(i*255)) for i in bg[:-1]], [str(int(i*255)) for i in boxbg[:-1]], [str(int(i*255)) for i in menubg[:-1]], [str(int(i*255)) for i in btn[:-1]]]==i[:4]:
+            self.color.height=250*scale
+            self.color.add_widget(self.icon)
+            self.got_check=self.color
+        self.color.bind(on_press=lambda instance: self.change_color_theme(instance, theme[0], theme[1], theme[2], theme[3], theme[4], theme[5]))
+
+        return self.color
+
+    def change_color_theme(self, instance, new_bg, new_boxbg, new_menubg, new_btn, new_primarycolor, new_secondarycolor):
+        self.got_check.remove_widget(self.icon)
+        self.got_check.height=200*scale
+        self.got_check=instance
+        instance.height=250*scale
+        instance.add_widget(self.icon)
+        self.color_palette_scroll.scroll_to(self.got_check)
+        ui.settings["current palette"]="; ".join([", ".join([str(int(i*255)) for i in new_bg][:-1]),", ".join([str(int(i*255)) for i in new_boxbg][:-1]),", ".join([str(int(i*255)) for i in new_menubg][:-1]),", ".join([str(int(i*255)) for i in new_btn][:-1]), ", ".join([str(int(i*255)) for i in new_primarycolor][:-1]), ", ".join([str(int(i*255)) for i in new_secondarycolor][:-1])])
+        self.dialog.open()
+
+    def create_preview(self, i):
+        self.font=MDFillRoundFlatIconButton(text=i, theme_icon_color='Custom', md_bg_color=btn, theme_text_color="Custom", text_color=secondarycolor, font_name=f"func/setting/fonts/{i}.ttf", font_size=17*scale)
+        if i==settings["fonts"]:
+            self.font.icon='check-circle'
+            self.font.icon_color=secondarycolor
+            self.got_font_check=self.font
+        self.font.bind(on_press=lambda instance: self.change_font(instance, i))
+
+        return self.font
+    
+    def change_font(self, instance, i):
+        self.got_font_check.icon=''
+        self.got_font_check=instance
+        instance.icon='check-circle'
+        instance.icon_color=secondarycolor
+        self.font_scroll.scroll_to(self.got_font_check)
+        ui.settings["fonts"]=i
+        self.dialog.open()
+
+    def back(self, instance):
+        sm.transition.direction = "right"
+        sm.current = 'first'
+
+    def on_touch(self, instance):
+        self.cre=MDLabel(text=f"""Chào mừng bạn đến với {settings["title"]}!
+
+Là nhà phát triển chính của {settings["title"]}, tôi muốn dành một chút thời gian để cảm ơn bạn đã sử dụng phần mềm của tôi.
+
+{settings["title"]} được xây dựng bằng ngôn ngữ lập trình Python và sử dụng thư viện giao diện KivyMD. Tôi đã dành rất nhiều thời gian và công sức để tạo ra một sản phẩm mà tôi hy vọng sẽ hữu ích cho bạn.
+
+Tôi rất biết ơn sự hỗ trợ và phản hồi của bạn. Những ý kiến đóng góp của bạn giúp tôi cải thiện {settings["title"]} và đảm bảo rằng nó đáp ứng nhu cầu của người dùng.
+
+Nếu bạn có bất kỳ câu hỏi hoặc yêu cầu nào, đừng ngần ngại liên hệ với tôi qua email. Tôi luôn sẵn lòng giúp đỡ và mong muốn nghe ý kiến từ bạn.
+
+Cảm ơn bạn đã sử dụng {settings["title"]}!
+
+Trân trọng,
+Nhật
+                """,
+                  font_style="Body2",
+                  size_hint=(1,None),
+                  height=25*scale,
+                  pos_hint={"center_x": 0.5},
+                  theme_text_color="Custom",
+                  text_color=primarycolor)
+        self.cre.bind(texture_size=self.cre.setter('text_size'))
+        self.cre.bind(texture_size=self.cre.setter('size'))
+        self.temp_scroll_box=ScrollView(size_hint=(1, 1), pos_hint={"center_x": 0.5}, do_scroll_x=False)
+        self.temp_scroll_box.add_widget(self.cre)
+        self.container=MDCard(orientation="vertical", spacing=20*scale, size_hint=(1,None), height=200*scale, padding=[10*scale, 10*scale, 10*scale, 10*scale])
+        self.container.radius=[i*scale for i in self.container.radius]
+        self.container.add_widget(self.temp_scroll_box)
+        self.credit=MDDialog(
+        title=f"Thư cảm ơn từ Nhà phát triển {settings['title']}",
+        type="custom",
+        content_cls=self.container,
+        buttons=[
+            MDFillRoundFlatButton(
+                text="Cập nhật dữ liệu",
+                md_bg_color=btn,
+                theme_text_color="Custom",
+                text_color=secondarycolor
+            ),
+            MDFillRoundFlatButton(
+                text="Đóng",
+                md_bg_color=btn,
+                theme_text_color="Custom",
+                text_color=secondarycolor
+            )            
+        ],
+        md_bg_color=boxbg
+        )
+        self.credit.buttons[0].bind(on_release=self.update_trigger)
+        self.credit.buttons[1].bind(on_release=self.credit.dismiss)
+        self.credit.open()
+        
+    def update_trigger(self, instance):
+        self.credit.dismiss()
+        self.screen=sm.get_screen('second')
+        self.screen.add_widget(self.overlay)
+        threading.Thread(target=self.update_).start()
+        
+    def touch_ignore():
+        pass
+    
+    def update_(self):
+        try:
+            if check_connection():
+                download_file("Whoop", "Whoop/func/data/tu_dien_nguon.txt", "func/data/tu_dien_nguon.txt")
+                download_file("Whoop", "Whoop/func/data/word.txt", "func/data/word.txt")
+                download_file("Whoop", "Whoop/func/data/grammar.txt", "func/data/grammar.txt")
+        except:
+            Clock.schedule_once(self.failed_)
+        else:
+            Clock.schedule_once(self.success_)
+            
+    def success_(self, instance):
+        self.screen.remove_widget(self.overlay)
+        self.success.open()
+        
+    def failed_(self, instance):
+        self.screen.remove_widget(self.overlay)
+        self.failed.open()
