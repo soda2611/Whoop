2fc869ecc53a4defba471df8091d3c101a975331
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-12-19 16:07:34 +0700

    Update __init__.py

--- Whoop/ui/home/__init__.py
+++ Whoop/ui/home/__init__.py
@@ -44,9 +44,7 @@ class home(MDBoxLayout, TouchBehavior):
             md_bg_color=boxbg
         )
 
-        self.label = Image(source=settings["banner"], size_hint=(0.9, None), pos_hint={'center_x': 0.5}, height=dp(50))
-
-        self.cautionlabel = MDLabel(text="Kết quả có thể sai do bộ dữ liệu chưa qua kiểm tra sàng lọc", font_style="Caption", halign='center', size_hint=(0.75, None), pos_hint={'center_x': 0.5, 'center_y': 0.5}, height=dp(30), theme_text_color="Custom", text_color=primarycolor)
+        self.label = Image(source=settings["banner"], size_hint=(0.9, None), pos_hint={'center_x': 0.5}, height=dp(60))
 
         self.resultlabel = MDLabel(text='', font_style="H6", halign='center', valign='middle', size_hint=(1, None), pos_hint={'center_x': 0.5, 'center_y': 0.5}, height=30)
 
