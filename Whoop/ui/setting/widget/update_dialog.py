6efa241e5238657c3875a1d5261e7bbd6df7ca60
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-12-22 22:36:41 +0700

    Update update_dialog.py

--- Whoop/ui/setting/widget/update_dialog.py
+++ Whoop/ui/setting/widget/update_dialog.py
@@ -14,7 +14,7 @@ class update_dialog(MDBottomSheet):
         self.early_access=MDCard(orientation="vertical", padding=[dp(10), dp(10), dp(10), dp(10)] , radius=[25, 25, 25, 25], md_bg_color=boxbg, size_hint=(0.5, 0.2), pos_hint={"top": 1}, ripple_behavior=True)
         self.early_access.add_widget(MDIcon(icon="clock-fast", pos_hint={"center_x": 0.5, "y": 1}, size_hint=(1, 1)))
         self.early_access.add_widget(MDLabel(text=f"[b]Truy cập sớm[/b]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
-        self.early_access.add_widget(MDLabel(text=f"[size={int(dp(12))}]Truy cập sớm vào phiên bản dữ liệu mới nhất, tuy nhiên tốc độ cập nhật chậm hơn[/size]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
+        self.early_access.add_widget(MDLabel(text=f"[size={int(dp(12))}]Truy cập sớm vào phiên bản dữ liệu ngoại tuyến mới nhất, tuy nhiên tốc độ cập nhật chậm hơn[/size]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
         self.container.add_widget(self.official)
         self.container.add_widget(self.early_access)
         self.add_widget(self.container)
