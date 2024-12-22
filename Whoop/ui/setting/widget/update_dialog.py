378bd32efda6f8c46d3065745206c5fe5ed1620b
soda2611 <145172441+soda2611@users.noreply.github.com>
2024-12-22 22:39:13 +0700

    Update update_dialog.py

--- Whoop/ui/setting/widget/update_dialog.py
+++ Whoop/ui/setting/widget/update_dialog.py
@@ -9,7 +9,7 @@ class update_dialog(MDBottomSheet):
         self.container=MDBoxLayout(spacing=dp(20), pos_hint={"center_x": 0.5})
         self.official=MDCard(orientation="vertical", padding=[dp(10), dp(10), dp(10), dp(10)] , radius=[25, 25, 25, 25], md_bg_color=boxbg, size_hint=(0.5, 0.2), pos_hint={"top": 1}, ripple_behavior=True)
         self.official.add_widget(MDIcon(icon="web", pos_hint={"center_x": 0.5, "y": 1}, size_hint=(1, 1)))
-        self.official.add_widget(MDLabel(text=f"[b]Dữ liệu chính thức[/b]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
+        self.official.add_widget(MDLabel(text=f"[b]Chính thức[/b]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
         self.official.add_widget(MDLabel(text=f"[size={int(dp(12))}]Truy cập vào phiên bản dữ liệu ngoại tuyến chính thức[/size]", pos_hint={"center_x": 0.5}, halign="center", valign="top", size_hint=(1, 1), markup=True))
         self.early_access=MDCard(orientation="vertical", padding=[dp(10), dp(10), dp(10), dp(10)] , radius=[25, 25, 25, 25], md_bg_color=boxbg, size_hint=(0.5, 0.2), pos_hint={"top": 1}, ripple_behavior=True)
         self.early_access.add_widget(MDIcon(icon="clock-fast", pos_hint={"center_x": 0.5, "y": 1}, size_hint=(1, 1)))
