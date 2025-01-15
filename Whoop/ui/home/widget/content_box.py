ab711c7e6cc758e0dd9731a32d23eedfbcb37939
soda2611 <145172441+soda2611@users.noreply.github.com>
2025-01-15 23:31:27 +0700

    Update content_box.py

--- Whoop/ui/home/widget/content_box.py
+++ Whoop/ui/home/widget/content_box.py
@@ -4,12 +4,16 @@ class content_box(MDCard):
     def __init__(self, text, **kwargs):
         super(content_box, self).__init__(**kwargs)
         self.md_bg_color=boxbg
-        self.padding=[dp(10),dp(10),dp(10),dp(10)]
+        self.padding=[dp(10), dp(10), dp(10), dp(10)]
         self.size_hint=(1, None)
         self.pos_hint={"center_x":0.5}
         self.radius=[dp(i) for i in self.radius]
         self.bind(minimum_height=self.setter('height'))
-        self.tilte_and_description_box=MDBoxLayout(orientation='vertical', size_hint=(0.8,None))
+        self._state_=True
+        self.morebutton=None
+        self._text_=text["definition"]
+        self.on_release=self.viewstate
+        self.tilte_and_description_box=MDBoxLayout(orientation='vertical', size_hint=(0.8,None), spacing=dp(10))
         self.tilte_and_description_box.bind(minimum_height=self.tilte_and_description_box.setter('height'))
         self.result_head_label=MDLabel(text=text["word"]+" ("+text["type"].lower()+")"+f'\n/{eng_to_ipa.convert(text["word"])}/', font_style="main", font_size=dp(18), size_hint=(0.9,None), pos_hint={"left":0}, theme_text_color="Custom", text_color=primarycolor)
         self.result_head_label.bind(texture_size=self.result_head_label.setter('text_size'))
@@ -24,9 +28,24 @@ class content_box(MDCard):
         self.tilte_and_description_box.add_widget(self.result_head_label)
         self.tilte_and_description_box.add_widget(self.result_label)
         self.add_widget(self.tilte_and_description_box)
+    
+    def viewstate(self):
+        if not self._state_:
+            if len(self._text_)>50:
+                self.result_label.text=self._text_[:50]+"..."
+            else:
+                self.result_label.text=self._text_
+            self.tilte_and_description_box.remove_widget(self.morebutton)
+            self.add_widget(self.morebutton)
+        else:
+            self.result_label.text=self._text_
+            self.remove_widget(self.morebutton)
+            self.tilte_and_description_box.add_widget(self.morebutton)
+            
+        self._state_=not self._state_
         
 class content_box_(MDCard):
-     def __init__(self, text, **kwargs):
+    def __init__(self, text, **kwargs):
         super(content_box_, self).__init__(**kwargs)
         self.md_bg_color=boxbg
         self.padding=[dp(10),dp(10),dp(10),dp(10)]
