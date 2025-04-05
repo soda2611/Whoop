d7cc9b12da1cd9554f19e2ae1f9b8b958013a739
soda2611 <145172441+soda2611@users.noreply.github.com>
2025-04-05 10:37:28 +0700

    Update necessary_function.py

--- Whoop/func/necessary_function.py
+++ Whoop/func/necessary_function.py
@@ -68,5 +68,40 @@ def spelling_checker_for_SOD(you, word=data()):
 
     return " ".join(result)
 
+def possible(word_input, word=data(), similarity_threshold=50):
+    word_input = word_input.lower()
+    possible_matches = []
+
+    if word_input not in word:
+        for y in word:
+            t = 0
+            if len(word_input) >= len(y):
+                for j in range(len(y)):
+                    if word_input[j] == y[j]:
+                        t += 1
+                    elif word_input[j] in y:
+                        l = y.find(word_input[j])
+                        if j - l == 1:
+                            t += 1
+            elif len(word_input) < len(y):
+                for j in range(len(word_input)):
+                    if word_input[j] == y[j]:
+                        t += 1
+                    elif word_input[j] in y:
+                        l = y.find(word_input[j])
+                        if l - j == 1:
+                            t += 1
+
+            k = t / len(y) * 100
+            h = t / len(word_input) * 100
+
+            if k >= similarity_threshold and h >= similarity_threshold:
+                possible_matches.append((y, k + h))
+
+    unique_matches = list({match[0]: match for match in possible_matches}.values())
+    unique_matches.sort(key=lambda match: match[1], reverse=True)
+
+    return [match[0] for match in unique_matches[:5]]
+
 if __name__=="__main__":
     pass
