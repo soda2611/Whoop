2e20c6ca907249d89fd4b5abafc0313522e08a02
torikachi <a3601000er@gmail.com>
2025-10-05 19:50:45 +0700

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById('textfield');
 const resultDiv = document.getElementById('result');
+const headDiv = document.getElementById('title')
 
 searchInput.addEventListener('keydown', async function(e) {
     if (e.key === 'Enter') {
@@ -17,18 +18,24 @@ searchInput.addEventListener('keydown', async function(e) {
             const data = await response.json();
             let html = "";
             data.forEach(entry => {
-                html += `<b>${entry.word}</b><br>`;
+                headDiv.innerText = capitalizeFirst(`${entry.word}`)
                 entry.meanings.forEach(meaning => {
-                    html += `<i>${meaning.partOfSpeech}</i>:<br>`;
+                    html += `<b>${meaning.partOfSpeech}</b>:<br>`;
                     meaning.definitions.forEach(def => {
                         html += `- ${def.definition}<br>`;
-                        if (def.example) html += `<span style="color:gray">Ví dụ: ${def.example}</span><br>`;
+                        if (def.example) html += `<span style="color:#0e42d3">• ${def.example}</span><br><br>`;
                     });
+                    html += `<br><hr><br>`
                 });
             });
+            html = html.slice(0, -12);
             resultDiv.innerHTML = html;
         } catch (e) {
             resultDiv.innerText = "Lỗi kết nối hoặc không tìm thấy từ.";
         }
     }
 });
+
+function capitalizeFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
