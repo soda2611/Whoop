4415e3088a2328434afd036d6cf31bf320472b8d
torikachi <a3601000er@gmail.com>
2025-10-06 21:56:39 +0700

    Updated

--- script.js
+++ script.js
@@ -9,35 +9,34 @@ searchInput.addEventListener('keydown', async function(e) {
 
         headDiv.innerText = "";
         resultDiv.innerText = "Đang tìm kiếm...";
-
         try {
             const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
             if (!response.ok) {
-                resultDiv.innerText = "Không tìm thấy từ.";
-                return;
+            resultDiv.innerText = "Không tìm thấy từ.";
+            return;
             }
             const data = await response.json();
+            const entry = data[0];
             let html = "";
-            data = data[0];
-            data.forEach(entry => {
-                headDiv.innerText = capitalizeFirst(`${entry.word}`)
-                html += `<span style="font-size: 15px">US: <b>${entry.phonetics[1].text}</b><br></span>`
-                if (entry.phonetics[0].text) html += `<span style="font-size: 15px">US: <b>${entry.phonetics[0].text}</b><br></span>`
-                html += `<br>`
-                entry.meanings.forEach(meaning => {
-                    html += `<b>${meaning.partOfSpeech}</b>:<br>`;
-                    meaning.definitions.forEach(def => {
-                        html += `- ${def.definition}<br>`;
-                        if (def.example) html += `<span style="color:#0e42d3">• ${def.example}</span><br><br>`;
-                        else html += `<br>`
-                    });
-                    html += `<br><hr><br>`
-                });
+            headDiv.innerText = capitalizeFirst(`${entry.word}`);
+            if (entry.phonetics[1] && entry.phonetics[1].text)
+            html += `<span style="font-size: 15px">US: <b>${entry.phonetics[1].text}</b><br></span>`;
+            if (entry.phonetics[0] && entry.phonetics[0].text)
+            html += `<span style="font-size: 15px">US: <b>${entry.phonetics[0].text}</b><br></span>`;
+            html += `<br>`;
+            entry.meanings.forEach(meaning => {
+            html += `<b>${meaning.partOfSpeech}</b>:<br>`;
+            meaning.definitions.forEach(def => {
+                html += `- ${def.definition}<br>`;
+                if (def.example) html += `<span style="color:#0e42d3">• ${def.example}</span><br><br>`;
+                else html += `<br>`;
+            });
+            html += `<br><hr><br>`;
             });
             html = html.slice(0, -12);
             resultDiv.innerHTML = html;
         } catch (e) {
-            resultDiv.innerText = "Lỗi kết nối hoặc không tìm thấy từ.";
+            resultDiv.innerText = "Lỗi vô định";
         }
     }
 });
